517d969ae8107b7a946504b4e84eb131577ace0f
170300STM <146239211+170300STM@users.noreply.github.com>
2023-09-29 10:34:35 +0300

    Add files via upload

---
+++ exercise1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout <<"Welcome to the wonderful world of C++!\n\n";
+    cout <<"This is my first C++ output statement\n\n";
+    cout <<"Soon, i willi leran to input data into my program";
+}
